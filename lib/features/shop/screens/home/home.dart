6fb113782b3aee9001eba9bb6ac4c920cbf3c930
Anuja005 <168641152+Anuja005@users.noreply.github.com>
2025-03-30 20:09:46 +0530

    searchbar and categories view

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,12 +1,20 @@
 import 'package:deal_nest/common/widgets/appbar/appbar.dart';
 import 'package:deal_nest/features/shop/screens/home/widgets/home_appbar.dart';
+import 'package:deal_nest/features/shop/screens/home/widgets/home_categories.dart';
 import 'package:deal_nest/utils/constants/colors.dart';
+import 'package:deal_nest/utils/constants/image_strings.dart';
+import 'package:deal_nest/utils/constants/sizes.dart';
 import 'package:deal_nest/utils/constants/text_strings.dart';
+import 'package:deal_nest/utils/device/device_utility.dart';
+import 'package:deal_nest/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
+import '../../../../common/widgets/custom_shapes/containers/search_container.dart';
+import '../../../../common/widgets/image_text_widgets/vertical_image_text.dart';
 import '../../../../common/widgets/products/cart/cart_menu_icon.dart';
+import '../../../../common/widgets/texts/section_heading.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -17,11 +25,34 @@ class HomeScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            ///header
+            ///header section
             MyPrimaryHeaderContainer(
               child: Column(
                 children: [
                   MyHomeAppBar(),
+                  SizedBox(height: TSizes.spaceBtwSections),
+
+                  ///Searchbar section
+                  MySearchContainer(text: 'Search in Store'),
+                  SizedBox(height: TSizes.spaceBtwSections),
+
+                  ///Categories section
+                  Padding(
+                    padding: EdgeInsets.only(left: TSizes.defaultSpace),
+                    child: Column(
+                      children: [
+                        ///Header
+                        MySectionHeading(
+                            title: 'Popular Categories',
+                            showActionButton: false,
+                            textColor: Colors.white),
+                        SizedBox(height: TSizes.spaceBtwItems),
+
+                        ///Categories
+                        MyHomeCategories(),
+                      ],
+                    ),
+                  )
                 ],
               ),
             ),
